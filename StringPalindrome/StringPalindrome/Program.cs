8c275f62f1cf18dc42e7b9cee582b79732d1bf99
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-21 16:36:36 +0100

    Created method that checks if string is palindrome

--- StringPalindrome/StringPalindrome/Program.cs
+++ StringPalindrome/StringPalindrome/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Insert some word:");
+var word = Console.ReadLine().ToLower(); ;
+
+bool isPalindrome = false;
+
+for (int i = 0, j = word.Length - 1; i < word.Length; i++, j--)
+{
+    if (word[i] != word[j])
+    {
+        isPalindrome = false;
+        break;
+    }
+    else
+    {
+        isPalindrome = true;
+    }
+}
+Console.WriteLine($"{isPalindrome}");
