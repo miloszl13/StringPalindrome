0aa85a0d01278706a06a7cbef6181ff9025b73c0
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-21 16:41:51 +0100

    Implemented SRP,OCP,DIP

---
+++ StringPalindrome/StringPalindrome/InputChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPalindrome
+{
+    public class InputChecker
+    {
+        IChecking _checking;
+        public InputChecker(IChecking checking)
+        {
+            _checking = checking;
+        }
+        public bool Check(string word)
+        {
+            return _checking.Check(word);
+        }
+    }
+}
--- StringPalindrome/StringPalindrome/Program.cs
+++ StringPalindrome/StringPalindrome/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+
+using StringPalindrome;
+
+var word = new UserInput().AskForWord();
+
+var checker = new InputChecker(new PalindromeChecker());
+var isPalindrome = checker.Check(word);
+
+Console.WriteLine($"{isPalindrome}");
